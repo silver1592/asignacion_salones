e907edd8177ac8a4c8d673a493adffde9a776bef
Fernando_lap <feral1592@gmail.com>
2017-05-17 14:21:53 -0500

    Vistas parciales SeleccionExcel y CargaExcel Falta anexar las vistas parciales y a simple vista parece que todo esta en orden (no se ha checado)

---
+++ asign01/InterfazWeb/Controllers/ImportacionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterfazWeb.Controllers
+{
+    public class ImportacionController : Controller
+    {
+        //
+        // GET: /Importacion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SeleccionExcel()
+        {
+            return View();
+        }
+
+        public ActionResult CargaExcel()
+        {
+            return View();
+        }
+
+    }
+}
