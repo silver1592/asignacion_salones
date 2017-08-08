44f4df6788dafc362c965bcaff0afca494e33e60
Fernando Alvarez Flores <feral1592@gmail.com>
2017-08-07 21:07:36 -0500

    Cabios en la interfaz (Home) Cambie el bloque de importacion de excel, tratando de hacer que sea un uso mas intuitivo

--- asign01/InterfazWeb_02/JS/Home/index.js
+++ asign01/InterfazWeb_02/JS/Home/index.js
@@ -1,13 +1,48 @@
-﻿$().ready(function () {
+﻿//#SeleccionExcel input[name='file'] ----> Boton para subir archivo
+
+$().ready(function () {
     //UX
     initialize();
     inicializaMenu();
 
+    //Comportamiento del boton para subir archivo
+    $("#SeleccionExcel input[name='file']").change(function () {
+        $(this).parent().submit();
+    })
 
+    //Comportamiento de la seleccion del archivo
+    $("#SeleccionExcel #archivos").change(function () {
+        /* Bloque donde obtiene la informacion de las hojas */
+        var excelName = $("#archivos option:selected").text();
+        var _url = $(".direccion #changeExcel").text().trim();
 
-    //Eventos
-    $("#archivos").change(changeExcel);
+        $("#hojas option").remove();
+        if (excelName != "---------") {
+            $.ajax({
+                type: "POST",
+                url: _url,
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify({ file: excelName }),
+                dataType: "json",
+                success: function (resultado) {
+                    $.each(resultado, function (index, item) {
+                        $("#hojas").append($("<option>" + item + "</option>"))
+                    });
+                },
+                error: ErrorFunction,
+                beforeSend: Wait,
+                complete: Continue,
+            });
+        }
+
+        /* Boque donde asigna la url a los botones*/
+        var urlDownloadButton = window.location.href + "/Archivos/" + excelName;
+        $("#SeleccionExcel #descarga").attr("href", urlDownloadButton);
+    });
+
+    //Comportamiento del boton para cargar la base de datos
     $("#SeleccionExcel #uploadExcel").click(uploadExcelToDB);
+
     $("#EjecutaOperaciones").click(Ejecuta);
 
     ActualizaOrigen();
