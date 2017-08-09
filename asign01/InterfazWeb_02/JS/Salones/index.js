6f7850fd7bac7725fd5f4beacea104a9321728e2
Fernando Alvarez Flores <feral1592@gmail.com>
2017-08-08 19:39:52 -0500

    Olvide agregarlo :P

---
+++ asign01/InterfazWeb_02/JS/Salones/index.js
@@ -0,0 +1,48 @@
+﻿$().ready(function () {
+    inicializaMenu();
+    ActualizaOrigen();
+
+    $("#salon").change(function () {
+        var salon = $("#salon option:selected").text().trim();
+
+        var url = $(".direccion #buscaSalon").text().trim();
+        var dt = JSON.stringify({ cve_salon: salon});
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            data: dt,
+            success: function (resultado) {
+                $("#salonList").empty();
+                $("#salonList").append(resultado);
+            },
+            beforeSend: Wait,
+            complete: Continue,
+            error: function (jqXHR, exception) {
+                alert(exception);
+            }
+        });
+    })
+
+    $("#empalmes").click(function () {
+        var url = $(".direccion #buscaSalon").text().trim();
+        var dt = JSON.stringify({ cve_salon: "empalmes" });
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            data: dt,
+            success: function (resultado) {
+                $("#salonList").empty();
+                $("#salonList").append(resultado);
+            },
+            beforeSend: Wait,
+            complete: Continue,
+            error: function (jqXHR, exception) {
+                alert(exception);
+            }
+        });
+    });
+});
