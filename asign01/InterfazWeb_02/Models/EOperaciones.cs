6d2bbb144d5fb223ea7e7342b89a148947a68e1b
Fernando Alvarez Flores <feral1592@gmail.com>
2018-01-14 22:50:39 -0600

    Faltaba de agregar este archivo

---
+++ asign01/InterfazWeb_02/Models/EOperaciones.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+enum EOperaciones : byte
+{
+    //-empalmes -preasignacion -otrosSemestres -algoritmo
+    [Description("Algoritmo Genetico")]
+    algoritmoGenetico = 1,
+    [Description("Revision de empalmes")]
+    empalmes,
+    [Description("Asignacion preferencial")]
+    preasignacion,
+    [Description("Asignacion de otros semestres")]
+    otrosSemestres
+}
